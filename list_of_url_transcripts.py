eb50a67ff28c3d146bf1784965f6ec17337380c2
Isaac Wu <isaacwu2000@gmail.com>
2024-08-01 18:31:33 +0000

    Completed the functions to get a list of video ids from a playlist url and a list of urls

---
+++ list_of_url_transcripts.py
@@ -0,0 +1,9 @@
+def get_list_of_vid_ids_from_list_of_urls(list_of_vid_urls):
+    list_of_vid_urls = list_of_vid_urls.split(',') # Converts the inputted comma-seperated list of URLs into a python list
+    video_IDs = []
+    for url in list_of_vid_urls:
+        if '&t=' in url:
+        vid_ID = url.split("&")[0]
+        vid_ID = url.split('=')[1]
+        video_IDs.append(vid_ID)
+    return video_IDs
