3941fdc8374ceb768196b2dfe2ea89005af2593b
Isaac Wu <isaacwu2000@gmail.com>
2024-08-01 18:50:49 +0000

    Finished making a mechanism to get a list of transcripts by title from a list of vid ids

--- channel_url_to_vid_ids.py
+++ channel_url_to_vid_ids.py
@@ -6,12 +6,9 @@ def get_channel_id_from_url(channel_url):
   }
   response = requests.get(channel_url, headers=headers)
 
-  # Check if the request was successful
-  if response.status_code == 200:
-    source_code = BeautifulSoup(response.text, 'html.parser').prettify()
-    return source_code.split('?channel_id=')[1].split('"',1)[0] #Finds the channel id from the source code of the youtube channel website and returns it
-  else:
-    return (f"Failed to retrieve the page. Status code: {response.status_code}")
+  source_code = BeautifulSoup(response.text, 'html.parser').prettify()
+  return source_code.split('?channel_id=')[1].split('"',1)[0] #Finds the channel id from the source code of the youtube channel website and returns it
+ 
 
 def list_of_vid_ids_from_channel_id(channel_id):
     import scrapetube
---
+++ vid_ids_to_transcripts.py
@@ -0,0 +1,30 @@
+def get_video_title_from_vid_id(vid_id):
+    import requests
+    from bs4 import BeautifulSoup
+
+    response = requests.get("https://www.youtube.com/watch?v=" + vid_id)
+    soup = BeautifulSoup(response.text) 
+    title = str(soup.find_all(name="title")[0]).replace("<title>","").replace("</title>","")
+    return title
+
+def vid_ids_to_list_of_transcripts(vid_IDs):
+    from youtube_transcript_api import YouTubeTranscriptApi
+    transcripts_by_vid_title = {}
+    for video_ID in vid_IDs:
+        try: # Tests if the video has a transcript to get
+            YouTubeTranscriptApi.get_transcript(video_ID)
+        except:
+            return (f"An error occured in getting the transcript 
+                    from the video: '{get_video_title_from_vid_id(video_ID)}'
+                    Please check if the video has a transcript.")
+        else:
+            youtube_transcript_dict_list = YouTubeTranscriptApi.get_transcript(video_ID)
+            youtube_transcript = ""
+
+            # Converts the dictionary transcript (time_in_the_video:text) 
+            # into a pure transcript (just text)
+            for i in youtube_transcript_dict_list:
+                youtube_transcript += " " + str(i["text"])
+            transcripts_by_vid_title["get_video_title_from_vid_id(video_ID)"] = youtube_transcript
+
+    return transcripts_by_vid_title
